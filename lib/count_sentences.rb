489dece3e0eefec6eb37120379598319805bd8ed
Justin Tooley <jstooley823@gmail.com>
2017-09-10 06:45:28 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,8 @@ class String
   end
 
   def count_sentences
-      para_array = []
-     para_array == self.split(".","?")
+     para_array = []
+     para_array = self.split(".","?")
      para_array.delete_if do |sentence|
        sentence.length < 2
      end
